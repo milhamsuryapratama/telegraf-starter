dc2d8292041e1342254a12d4149bf07166db3219
ilham <blogsayailham@gmail.com>
2019-09-08 00:47:41 +0700

    metode baru untuk new dan left chat member

--- routes.js
+++ routes.js
@@ -9,10 +9,18 @@ const commands = [
 
 const events = [
     {
-        event: 'new_chat_members'
+        event: 'new_chat_members',
+        action: ctx => {
+            const username = ctx.update.message.new_chat_members[0].username;
+            ctx.reply(`halo kaka @${username} selamat datang di grup bucin`);
+        },
     },
     {
-        event: 'left_chat_member'
+        event: 'left_chat_member',
+        action: ctx => {
+            const username = ctx.update.message.left_chat_member.username;
+            ctx.reply(`Loh kak @${username} kok keluar ?`);
+        },
     }
 ];
 
