3b618fc3eff624d3624bca97f1044ed26ff2b798
ilham <blogsayailham@gmail.com>
2019-09-13 20:59:36 +0700

    memasukkan bot command ke routing

--- app.js
+++ app.js
@@ -8,32 +8,31 @@ const app = express();
 const bot = new Telegraf(config.botToken);
 
 app.get('/', (req, res, next) => {
-    res.json({ "pesan": "sukses" });
+    // res.json({ "pesan": "sukses" });
+    bot.start(ctx => {
+        const username = ctx.update.message.from.username;
+        ctx.reply(`Halo kaka @${username}, selamat datang di grup bucin`)
+    });
+
+    routes.forEach(item => {
+        if (item.command) {
+            bot.command(item.command, (ctx) => {
+                const message = ctx.update.message.text;
+                const username = ctx.update.message.from.username;
+                ctx.reply(`halo kak @${username}, kaka sekarang menuju menu ${message}`);
+            });
+        }
+
+        if (item.event) {
+            bot.on(item.event, (ctx) => {
+                item.action(ctx);
+            });
+        }
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('success');
 })
-
-bot.start(ctx => {
-    const username = ctx.update.message.from.username;
-    ctx.reply(`Halo kaka @${username}, selamat datang di grup bucin`)
-});
-
-routes.forEach(item => {
-    if (item.command) {
-        bot.command(item.command, (ctx) => {
-            const message = ctx.update.message.text;
-            const username = ctx.update.message.from.username;
-            ctx.reply(`halo kak @${username}, kaka sekarang menuju menu ${message}`);
-        });
-    }
-
-    if (item.event) {
-        bot.on(item.event, (ctx) => {
-            item.action(ctx);
-        });
-    }
-});
 
 bot.launch();
