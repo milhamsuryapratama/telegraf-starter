6b1336de2348f5352595e19b45e5165dce3bf1b2
ilham <blogsayailham@gmail.com>
2019-10-05 01:42:29 +0700

    mengeluarkan kode bot pada router

--- app.js
+++ app.js
@@ -8,31 +8,28 @@ const app = express();
 
 const bot = new Telegraf(config.botToken);
 
-app.get('/', (req, res, next) => {
-    // res.json({ "pesan": "sukses" });
-    bot.start(ctx => {
-        const username = ctx.update.message.from.username;
-        ctx.reply(`Halo kaka @${username}, selamat datang di grup bucin`)
-    });
-
-    routes.forEach(item => {
-        if (item.command) {
-            bot.command(item.command, (ctx) => {
-                const message = ctx.update.message.text;
-                const username = ctx.update.message.from.username;
-                ctx.reply(`halo kak @${username}, kaka sekarang menuju menu ${message}`);
-            });
-        }
-
-        if (item.event) {
-            bot.on(item.event, (ctx) => {
-                item.action(ctx);
-            });
-        }
-    });
-
-    bot.launch();
+bot.start(ctx => {
+    const username = ctx.update.message.from.username;
+    ctx.reply(`Halo kaka @${username}, selamat datang di grup bucin`)
 });
+
+routes.forEach(item => {
+    if (item.command) {
+        bot.command(item.command, (ctx) => {
+            const message = ctx.update.message.text;
+            const username = ctx.update.message.from.username;
+            ctx.reply(`halo kak @${username}, kaka sekarang menuju menu ${message}`);
+        });
+    }
+
+    if (item.event) {
+        bot.on(item.event, (ctx) => {
+            item.action(ctx);
+        });
+    }
+});
+
+bot.launch();
 
 cron.schedule('*/30 * * * *', () => {
     // console.log('Runing a job at 01:00 at America/Sao_Paulo timezone');
