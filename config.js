54c5c94df42578429e5f8c86a4a6bd08c50e4adf
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-11-18 00:01:50 +0700

    hapus bot token

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    //654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0 (bucin)
-    //660251426:AAGz7s3-VyZeJI9USqqMyAJVtAwIGnlS7nQ (ilhamsuryabot)
-    botToken: "654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0"
+    botToken: "bot token"
 }
