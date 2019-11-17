e91fc1a7e144e30a53a71b2cce978b9fd2d695d1
ilham <blogsayailham@gmail.com>
2019-11-17 23:16:17 +0700

    mengganti bot token

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    botToken: "660251426:AAGz7s3-VyZeJI9USqqMyAJVtAwIGnlS7nQ"
+    //654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0 (bucin)
+    //660251426:AAGz7s3-VyZeJI9USqqMyAJVtAwIGnlS7nQ (ilhamsuryabot)
+    botToken: "654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0"
 }
