b2cb2da2a32823d0caca4211f54d75aacd17274b
ilham <blogsayailham@gmail.com>
2019-11-17 23:31:24 +0700

    testing ganti token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     //654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0 (bucin)
     //660251426:AAGz7s3-VyZeJI9USqqMyAJVtAwIGnlS7nQ (ilhamsuryabot)
-    botToken: "654478605:AAGp_n1qG0LSkFv-Bz69msBLV--vq-CyZb0"
+    botToken: "660251426:AAGz7s3-VyZeJI9USqqMyAJVtAwIGnlS7nQ"
 }
