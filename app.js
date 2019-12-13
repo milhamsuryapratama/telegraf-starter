994c88b6df7eabdc0d55fe4339639d80f96cb46d
milhamsuryapratama <ilhamsuryapratama.github@gmail.com>
2019-12-13 22:46:01 +0700

    test reply message id (2)

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ bot.start(ctx => {
     const username = ctx.update.message.from.username;
     const messageId = ctx.update.message.message_id;
     // ctx.reply(`Halo kaka @${username}, selamat datang di grup bucin`);
-    bot.telegram.sendMessage(chatId, `Halo kaka @${username}, selamat datang di grup bucin`, messageId);
+    bot.telegram.sendMessage(chatId, `Halo kaka @${username}, selamat datang di grup bucin`, [reply_to_message_id => messageId]);
 });
 
 routes.forEach(item => {
